c8273cddb94904db1a28253a77b632a6461d16df
nikhil.vavs <nikhil@flocare.health>
2018-10-24 20:19:36 +0530

    Fixing migration helper for old apps

--- phi/migration_helpers.py
+++ phi/migration_helpers.py
@@ -3,6 +3,8 @@ class MigrationHelpers:
     @staticmethod
     def handle_miles_migration(visit_miles):
         odometer_start = visit_miles.get('odometerStart', None)
-        odometer_end = visit_miles.get('odometerStart', None)
+        odometer_end = visit_miles.get('odometerEnd', None)
+        miles_comments = visit_miles.get('milesComments', '')
         if odometer_end is not None and odometer_start is not None:
             visit_miles['computedMiles'] = float(odometer_end) - float(odometer_start)
+            visit_miles['milesComments'] = miles_comments + ' Odometer Start: ' + str(odometer_start) + ' Odometer End :' + str(odometer_end)
