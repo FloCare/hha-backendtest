74f5507b4484a2c598bdb9c7cda2f381ae870e27
Karthik <karthik@flocare.health>
2018-11-12 11:17:14 +0530

    FC-128 : Places added to the response payload of visits by Org

--- phi/response_serializers.py
+++ phi/response_serializers.py
@@ -105,6 +105,15 @@ class PhysicianResponseSerializer(serializers.ModelSerializer):
         fields = ('physicianID', 'npi', 'firstName', 'lastName', 'phone1', 'phone2', 'fax')
 
 
+class PlacesResponseSerializer(serializers.ModelSerializer):
+    placeID = serializers.UUIDField(source='uuid', required=False)
+    address = AddressIDWithLatLngSerializer()
+
+    class Meta:
+        model = models.Episode
+        fields = ('placeID', 'address')
+
+
 class EpisodeWithPatientsResponseSerializer(serializers.ModelSerializer):
     episodeID = serializers.UUIDField(source='uuid', required=False)
     patient = PatientWithAddressSerializer()
@@ -150,6 +159,7 @@ class VisitForOrgResponseSerializer(serializers.ModelSerializer):
     visitID = serializers.UUIDField(source='id')
     userID = serializers.UUIDField(source='user_id')
     episode = EpisodeWithPatientsResponseSerializer()
+    place = PlacesResponseSerializer()
     timeOfCompletion = serializers.DateTimeField(source='time_of_completion', required=False)
     isDone = serializers.BooleanField(source='is_done', required=False)
     isDeleted = serializers.BooleanField(source='is_deleted', required=False)
@@ -163,7 +173,7 @@ class VisitForOrgResponseSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = models.Visit
-        fields = ('visitID', 'userID', 'episode', 'timeOfCompletion', 'isDone', 'isDeleted', 'plannedStartTime')
+        fields = ('visitID', 'userID', 'episode', 'place', 'timeOfCompletion', 'isDone', 'isDeleted', 'plannedStartTime')
 
 
 class VisitMilesResponseSerializer(serializers.ModelSerializer):
