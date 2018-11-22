bd75420318189a366faf09d9c63e53a68f31f849
nikhil.vavs <nikhil@flocare.health>
2018-11-23 00:19:28 +0530

    Making first_name as default sort

--- user_auth/views.py
+++ user_auth/views.py
@@ -36,6 +36,8 @@ class UserOrganizationView(APIView):
         if sort_field:
             if sort_field not in allowed_fields:
                 sort_field = 'first_name'
+        else:
+            sort_field = 'first_name'
         query = query_params.get('query', None)
         size = query_params.get('size', None)
         try:
@@ -63,9 +65,8 @@ class UserOrganizationView(APIView):
 
             # Todo: Improve Sorting logic - use DRF builtin
             query, sort_field, sort_order, size = self.parse_query_params(request.query_params)
-            query_params = request.query_params
-            if 'sort' in query_params:
-                sort_field = query_to_db_field_map.get(query_params['sort'], sort_field)
+            if sort_field:
+                sort_field = query_to_db_field_map.get(sort_field)
                 if sort_order == 'DESC':
                     sort_field = '-' + sort_field
 
